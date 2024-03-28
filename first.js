6e6dc7ebfe113d0beadcbbefa079c4b43584e815
rakesh1436 <sanjayrakeshgupta@gmail.com>
2024-03-29 01:43:59 +0530

    add

--- first.js
+++ first.js
@@ -2,6 +2,9 @@ console.log("sanjay");
 const fullname="sanjaygupta  "
 const age=23;
 console.log(age+1)
+x=null;
+y=undefined;
+Boolean=true;
 for (let index = 0; index < fullname.length; index++) {
 console.log(fullname+index);
     
