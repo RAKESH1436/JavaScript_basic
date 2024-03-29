4d62a2194b95a496dbe7369f612022b305bab64b
rakesh1436 <sanjayrakeshgupta@gmail.com>
2024-03-29 18:15:39 +0530

    add

--- first.js
+++ first.js
@@ -1,11 +1,18 @@
 console.log("sanjay");
-const fullname="sanjaygupta  "
-const age=23;
-console.log(age+1)
-x=null;
-y=undefined;
-Boolean=true;
+const fullname = "sanjaygupta  ";
+const age = 23;
+console.log(age + 1);
+x = null;
+y = undefined;
+Boolean = true;
 for (let index = 0; index < fullname.length; index++) {
-console.log(fullname+index);
-    
+  console.log(fullname + index);
 }
+const student = {
+  name: "sanhay",
+  age: 24,
+  place: "silli",
+  cpga: 8.3,
+  inPass: true,
+};
+
