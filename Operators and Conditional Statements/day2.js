eb6331a1df1cd417db1e1cb90bff851104bdb591
rakeshkumar1436 <sanjay.rakesh.gupta@gmail.com>
2024-03-29 22:35:35 +0530

    new

--- Operators and Conditional Statements/day2.js
+++ Operators and Conditional Statements/day2.js
@@ -1,11 +1,19 @@
 //console.log("hello world")
-let a = 5;
-let b = 2;
-let c = ("a+b=", a + b);
-console.log("a-b=", a - b);
-console.log("a*b=", a * b);
-console.log("a/b=", a / b);
-console.log(c);
-console.log("a%b=", a % b);
-console.log("a**b=", a ** b);
+// let a = 5;
+// let b = 2;
+// let c = ("a+b=", a + b);
+// console.log("a-b=", a - b);
+// console.log("a*b=", a * b);
+// console.log("a/b=", a / b);
+// console.log(c);
+// console.log("a%b=", a % b);
+// console.log("a**b=", a ** b);
+let a=5;
+let b=8;
+a=5+1;
+console.log("a=", a);
+b=b++;
+console.log("b=", b);
+
+
 
