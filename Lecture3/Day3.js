2c96352333008132f1a3bc3929e0543ff885600a
rakesh1436 <sanjayrakeshgupta@gmail.com>
2024-04-02 01:07:09 +0530

    add

--- Lecture3/Day3.js
+++ Lecture3/Day3.js
@@ -14,9 +14,37 @@
 //   console.log("sanjay");
 //   i++;
 // }
-let e=3;
+// let e=3;
 
-do {
-  console.log("sanjay ");
-  e++;
-} while (e<=2);
+// do {
+//   console.log("sanjay ");
+//   e++;
+// } while (e<=2);
+
+// let str="sanjay";
+// let size =0;
+// for(let i of str){
+//   console.log("i",i);
+//   size++;
+// }
+// console.log(size)
+
+let student ={
+  name:"sanjay",
+  age:20,
+  cgpa:7.5,
+  isPass:true
+
+};
+for(let key in student){
+  console.log("key =",key,student[key]);
+}
+
+let s=100;
+for(let i=0;i<=s;i++){
+  if(i%2==0){
+    console.log(i);
+
+  }
+
+}
