461ea14248b494c08720304966a8b25f7c60d919
rakesh1436 <sanjayrakeshgupta@gmail.com>
2024-04-01 22:15:45 +0530

    add

--- Lecture3/Day3.js
+++ Lecture3/Day3.js
@@ -1,1 +1,10 @@
-console.log("sanjay");
+const a = 100;
+for (let index = 0; index <= a; index++) {
+  const element = index;
+}
+let sum = 0;
+for (let index = 0; index <= 6; index++) {
+  sum = sum + index;
+}
+console.log("sum", sum);
+console.log("loop has ended");
