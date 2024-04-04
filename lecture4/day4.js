1825714cebd8af8d553d39db1223789973c91337
rakesh1436 <sanjayrakeshgupta@gmail.com>
2024-04-04 18:03:22 +0530

    add

--- lecture4/day4.js
+++ lecture4/day4.js
@@ -1,35 +1,44 @@
-console.log("sanjay");
-let heroes=["sanjay","rakesh","sahshauh"]
-console.log(heroes);
-let marks=[21,21,2323,323,232,3]
-console.log(marks);
-let info=["rahul",65,"sahg"];
-console.log(info)
-let mark={
-    stu1:97,
-    stu2:82,
-}
-console.log(mark);
-console.log(marks.length);
-let hero=["sanjay","rakesh","sahshauh"]
-let hero1=["sanjay","rakesh","sahshauh"]
-let hero2=["sanjay","rakesh","sahshauh"]
-let a=[hero,hero1,hero2];
+// console.log("sanjay");
+// let heroes=["sanjay","rakesh","sahshauh"]
+// console.log(heroes);
+// let marks=[21,21,2323,323,232,3]
+// console.log(marks);
+// let info=["rahul",65,"sahg"];
+// console.log(info)
+// let mark={
+//     stu1:97,
+//     stu2:82,
+// }
+// console.log(mark);
+// console.log(marks.length);
+// let hero=["sanjay","rakesh","sahshauh"]
+// let hero1=["sanjay","rakesh","sahshauh"]
+// let hero2=["sanjay","rakesh","sahshauh"]
+// let a=[hero,hero1,hero2];
 // console.log(a[2]);
 // console.log(a[hero[2]="sanj"])
 // console.log(a)
 //loping over an arrray 
 //print all the element
-for (let index = 0; index < a.length; index++) {
-    //const element = Array[index];
-    console.log(hero2[index]);
+// for (let index = 0; index < a.length; index++) {
+//     //const element = Array[index];
+//     console.log(hero2[index]);
     
-}
+// }
+// for (let iterator of a) {
+//     console.log(iterator)
+    
+// }
+// console.log(hero[1])
 
 
+let cites=["lalpur","ranchi","silli","london"];
+for (let city of cites) {
+    console.log(city);
+   
+        
+    }
 
 
-
-console.log(hero[2])
 
 
