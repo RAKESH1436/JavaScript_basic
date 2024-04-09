a1109c7bb6ddc54ea5dbee424f14bbe970fb1197
rakesh1436 <sanjayrakeshgupta@gmail.com>
2024-04-09 21:49:26 +0530

    add

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 DAY 1
 ![image](https://github.com/RAKESH1436/JavaScript_basic/assets/60301255/bdaab550-216a-4cc4-aab8-6846a8737cc0)
 
+
+
+
+
--- lecture4/day4.js
+++ lecture4/day4.js
@@ -18,27 +18,45 @@
 // console.log(a[2]);
 // console.log(a[hero[2]="sanj"])
 // console.log(a)
-//loping over an arrray 
+//loping over an arrray
 //print all the element
 // for (let index = 0; index < a.length; index++) {
 //     //const element = Array[index];
 //     console.log(hero2[index]);
-    
+
 // }
 // for (let iterator of a) {
 //     console.log(iterator)
-    
+
 // }
 // console.log(hero[1])
 
+// let cites=["lalpur","ranchi","silli","london","somadih"];
+// for (let city of cites) {
+//     console.log(city.length,city.toUpperCase());
 
-let cites=["lalpur","ranchi","silli","london"];
-for (let city of cites) {
-    console.log(city);
-   
-        
-    }
+//     }
+// let items = [20, 32, 10, 87, 80];
+// let i = 0;
+// for (let val of items) {
+//   let offer = val / 10;
+//   items[i] = items[i] - offer;
+//   console.log(`values=${items[i]}`);
+//   i++;
+// }
+// for (let i = 0; i < items.length; i++) {
+//   let offer = items[i] / 10;
+//   items[i] -= offer;
+//   console.log(`values = ${items[i]}`);
+// }
 
-
-
+let fooditem = ["potato", "apple", "litchi", "tomato"];
+let mark=[56,76,77,65,75];
+fooditem.push("mango");
+console.log(fooditem);
+let deleted=fooditem.pop();
+fooditem.pop();
+console.log(deleted);
+console.log(fooditem.toString());
+console.log(mark.toString());
 
