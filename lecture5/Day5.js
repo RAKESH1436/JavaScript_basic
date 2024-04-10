dd8b78af1682d66f85299d5251559cab924244fe
rakesh1436 <sanjayrakeshgupta@gmail.com>
2024-04-11 01:36:31 +0530

    arrowfunction

--- lecture5/Day5.js
+++ lecture5/Day5.js
@@ -1,19 +1,55 @@
 //console.log("sanjay");
-function name() {
-    console.log("sanjay");
-    console.log("sanju");
+// function name() {
+//   console.log("sanjay");
+//   console.log("sanju");
+// }
+
+// let a = name();
+// function any(a) {}
+// any(a);
+// function Mynane(msg, n) {
+//   console.log(msg * n);
+// }
+// Mynane("hii i m  sanjay", 100);
+
+// function add(x, y) {
+//   console.log(x + y);
+// }
+// add(4, 7);
+// function sum(x,y){
+//   s=x+y;
+//   return s;
+// }
+// console.log(sum(6,7))
+//arrowfunation
+// function sum(a, b) {
+//   return a + b;
+// }
+
+// const arrowSum = (a, b) => {
+//   console.log(a + b);
+// };
+// function multi(a, b) {
+//   return a * b;
+// }
+
+function countVowel(str) {
+  let count = 0;
+  for (const char of str) {
+    console.log(char);
+    if (
+      char === "a" ||
+      char === "e" ||
+      char === "i" ||
+      char === "o" ||
+      char === "u"
+    ) {
+      count++;
+    }
+
+  }
+console.log(count);
+
 
 }
-
-
-let a= name();
-function any(a) {
-  
-}
-any(a);
-
-function Mynane(msg){
-    console.log(msg);
-}
-Mynane("love");
-
+console.log(countVowel("abc"))
