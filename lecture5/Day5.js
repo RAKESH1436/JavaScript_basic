621c3a2ad5b095567690802e1b31964b148a41cb
rakesh1436 <sanjayrakeshgupta@gmail.com>
2024-04-10 22:06:25 +0530

    add

--- lecture5/Day5.js
+++ lecture5/Day5.js
@@ -1,1 +1,19 @@
-console.log("sanjay"); 
+//console.log("sanjay");
+function name() {
+    console.log("sanjay");
+    console.log("sanju");
+
+}
+
+
+let a= name();
+function any(a) {
+  
+}
+any(a);
+
+function Mynane(msg){
+    console.log(msg);
+}
+Mynane("love");
+
