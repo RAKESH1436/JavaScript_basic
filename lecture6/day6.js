a7edd6fa64f6393565730c0fab3c48ad1e1846e5
rakesh1436 <sanjayrakeshgupta@gmail.com>
2024-04-12 23:33:14 +0530

    add

--- lecture6/day6.js
+++ lecture6/day6.js
@@ -2,6 +2,17 @@ console.log("sanjay");
 alert("thanks for visit");
 window.console.log("reeyetyety");
 console.log(document.body);
-document.body.style.background="red" ;
+document.body.style.background="green" ;
 let heading=document.getElementById("heading");
 console.dir(heading);
+let headung =document.getElementsByClassName("heading-class");
+console.log(headung)
+
+let parahs=document.getElementsByTagName("p");
+console.log(parahs);
+
+let elements=document.querySelector("p");
+console.log(elements);
+let allelement=document.querySelectorAll("p")
+console.log(allelement);
+
