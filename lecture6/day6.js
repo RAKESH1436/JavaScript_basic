06cb6dd253a083a97fb2ad26ce9cad5746a6d487
rakesh1436 <sanjayrakeshgupta@gmail.com>
2024-04-13 17:25:28 +0530

    new

--- lecture6/day6.js
+++ lecture6/day6.js
@@ -1,15 +1,15 @@
-console.log("sanjay");
-alert("thanks for visit");
-window.console.log("reeyetyety");
-console.log(document.body);
-document.body.style.background="green" ;
-let heading=document.getElementById("heading");
-console.dir(heading);
-let headung =document.getElementsByClassName("heading-class");
-console.log(headung)
+// console.log("sanjay");
+// alert("thanks for visit");
+// window.console.log("reeyetyety");
+// console.log(document.body);
+// document.body.style.background="green" ;
+// let heading=document.getElementById("heading");
+// console.dir(heading);
+// let headung =document.getElementsByClassName("heading-class");
+// console.log(headung)
 
-let parahs=document.getElementsByTagName("p");
-console.log(parahs);
+// let parahs=document.getElementsByTagName("p");
+// console.log(parahs);
 
 // let elements=document.querySelector(".p");//class
 // console.log(elements);
@@ -19,7 +19,7 @@ console.log(parahs);
 // console.log(elements);
 // let allelement=document.querySelectorAll("p")
 // console.log(allelement);
-let div=document.querySelector("div");
-console.dir(div)
+// let div=document.querySelector("div");
+// console.dir(div)
 
 
