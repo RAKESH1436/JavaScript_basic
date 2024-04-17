c38fe9104118385d595801a28699a0496b6bf0de
rakesh1436 <sanjayrakeshgupta@gmail.com>
2024-04-17 22:20:33 +0530

    add

---
+++ lecture7/day7.js
@@ -0,0 +1,25 @@
+let btn1 = document.querySelector("#btn1");
+
+btn1.onclick = (e) => {
+  console.log(e);
+//   console.log(e.type);
+//   console.log(e.target);
+//   console.log(e.clientX, e.clientY);
+
+  // let a=25;
+  // a++;
+  // console.log(a);
+};
+btn1.addEventListener("click", () => {
+  console.log("buttom was clicked");
+  
+});
+
+let div = document.querySelector("div");
+div.onmouseover = (e) => {
+    
+//   console.log("you are indide div");
+//   console.log(e.type);
+//   console.log(e.target);
+//   console.log(e.clientX, e.clientY);
+};
