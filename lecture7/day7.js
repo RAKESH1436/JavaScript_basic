1fad08ea5cfa4b7bed7c468b62ce46c586bc5e29
rakesh1436 <sanjayrakeshgupta@gmail.com>
2024-04-18 01:14:42 +0530

    add

--- lecture7/day7.js
+++ lecture7/day7.js
@@ -1,23 +1,44 @@
-let btn1 = document.querySelector("#btn1");
+// let btn1 = document.querySelector("#btn1");
 
-btn1.onclick = (e) => {
-  console.log(e);
-  //   console.log(e.type);
-  //   console.log(e.target);
-  //   console.log(e.clientX, e.clientY);
+// btn1.onclick = (e) => {
+//   console.log(e);
+//   //   console.log(e.type);
+//   //   console.log(e.target);
+//   //   console.log(e.clientX, e.clientY);
 
-  // let a=25;
-  // a++;
-  // console.log(a);
-};
-btn1.addEventListener("click", () => {
-  console.log("buttom was clicked");
-});
+//   // let a=25;
+//   // a++;
+//   // console.log(a);
+// };
+// btn1.addEventListener("click", () => {
+//   console.log("buttom was clicked");
+// });
+// btn1.addEventListener("click", () => {
+//   console.log("2buttom was clicked");
+// });
+// const handler4 = () => {
+//   console.log("new handler");
+// };
+// btn1.addEventListener("click", handler4);
 
-let div = document.querySelector("div");
-div.onmouseover = (e) => {
+// let div = document.querySelector("div");
+// div.onmouseover = (e) => {
+//   console.log(e);
+
   //   console.log("you are indide div");
-  //   console.log(e.type);
   //   console.log(e.target);
   //   console.log(e.clientX, e.clientY);
-};
+//};
+
+let modebtn = document.querySelector("#mode");
+let current="light";
+modebtn.addEventListener("click", () => {
+  if(current==="light"){
+    current="dark";
+
+  }else{
+    current="light"
+
+  }
+  console.log(current);
+});
