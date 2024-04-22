f71515967cd311b3925bacd85457675f2b228aa3
rakesh1436 <sanjayrakeshgupta@gmail.com>
2024-04-22 22:20:50 +0530

    add

--- hitesh/test.js
+++ hitesh/test.js
@@ -3,3 +3,9 @@ let strinnum=String(number);
 console.log(typeof strinnum);
 //change the number to string
 
+let date=new Date();
+let dat=date.toString();
+console.log(dat);
+console.log(date.toJSON())
+console.log(date.toDateString())
+console.log(date.toLocaleTimeString())
