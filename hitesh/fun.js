7da42e5a762753abb0cce0e8759f46ab277dc48f
rakesh1436 <sanjayrakeshgupta@gmail.com>
2024-04-25 20:18:59 +0530

    add

---
+++ hitesh/fun.js
@@ -0,0 +1,16 @@
+function saymycode() {
+  console.log("sanjay");
+}
+
+saymycode();
+
+// function add(num1,num2){
+// console.log(num1+num2);
+// }
+
+function add(num1, num2) {
+  let result = num1 + num2;
+  return result;
+}
+let result = add(4, 7);
+console.log(result);
