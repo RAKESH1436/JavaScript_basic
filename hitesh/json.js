3aea951f6fb87cb52c34fc4f3896938b67da457e
rakesh1436 <sanjayrakeshgupta@gmail.com>
2024-04-25 16:38:26 +0530

    json

---
+++ hitesh/json.js
@@ -0,0 +1,14 @@
+const list = {
+  courname: "js in hindi",
+  price: "7878",
+  courseIn: "hitesh",
+};
+const { courseIn :inter} = list;
+console.log(inter);
+///json
+// {
+//     "name":"sanjay",
+//     "course":"js in hindi",
+//     "price":"w2873"
+
+// }
