7877969cec49b432e5f88e14d7ce69014ef21126
rakesh1436 <sanjayrakeshgupta@gmail.com>
2024-04-26 22:02:02 +0530

    add

--- hitesh/fun.js
+++ hitesh/fun.js
@@ -15,12 +15,68 @@
 // let result = add(4, 7);
 // console.log(result);
 
-function loginUserMessage(username) {
-  if (username === undefined) {
-    console.log("pls enter the usename ");
-  } else {
-    return `${username} just logged in`;
+// function loginUserMessage(username="sanjay") {
+//   if (!username) {
+//     console.log("pls enter the usename ");
+//     return;
+//   } else {
+//     return `${username} just logged in`;
+//   }
+// }
+// let s = loginUserMessage();
+//  console.log(s);
+
+//  function calAdd(...num1){
+//     return num1;
+
+//  }
+//  let sa=calAdd(234,54343,75454);
+//  console.log(sa);
+//  let saa=calAdd(6567);
+//  console.log(saa);
+
+//  const user={
+//     name:"sanjay",
+//     price:199
+//  }
+// console.log(user.name);
+// console.log(user.price);
+
+// function hanbleobje(anyobj){
+//     console.log(`username is ${anyobj.name} and price ${anyobj.price}`)
+// }
+// hanbleobje({
+//     name:"sanju",
+//     price:"3454"
+// });
+
+// const myNewArr=[200,400,800,380];
+
+// function returnSecValue(getArray){
+//     return getArray[2];
+// }
+
+// console.log(returnSecValue(myNewArr));
+
+if (true) {
+  const usename = "rakesh";
+  if (usename === "rakesh") {
+    const website = " is very good boy !!";
+    console.log(usename + website);
   }
+ // console.log(website);
 }
-// let s = loginUserMessage("rakesh");
-// console.log(s);
+
+//console.log(usename);
+console.log(add(5));
+
+
+function add(num){
+    return num +1;
+
+}
+console.log(this.addtwo(5));
+
+const addtwo = function (num){
+    return num +2
+}
---
+++ hitesh/scop.js
@@ -0,0 +1,24 @@
+
+
+// let a=500;
+// if(true){
+//     let a=10;
+//     const b=30;
+//     console.log("Inter:",a)
+
+// }
+// //console.log(a); 
+// console.log(a);
+
+
+function one (){
+    const usename="sanjay"
+    function two(){
+        const website="youtube"
+        console.log(usename);
+        console.log(website);
+    }
+    //console.log(website);
+    two();
+}
+one()
