0cc3bc79d77c82d016997427b7ad1bd643005205
rakesh1436 <sanjayrakeshgupta@gmail.com>
2024-04-27 01:33:32 +0530

    add

--- hitesh/arrow.js
+++ hitesh/arrow.js
@@ -3,9 +3,17 @@ const user = {
   price: 787,
   welcomeMessage: function () {
     console.log(`${this.username}, welcome to website`);
+    console.log(this);
   },
   
 };
-user.welcomeMessage();
-user.username="rakesh";
-user.welcomeMessage();
+// user.welcomeMessage();
+// user.username="rakesh";
+// user.welcomeMessage();
+console.log(this);
+
+function chai(){
+    let username="don";
+    console.log(this.username);
+}
+chai();
