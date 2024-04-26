96670f66e16f6a89715629419e15fc9079c441a0
rakesh1436 <sanjayrakeshgupta@gmail.com>
2024-04-27 01:55:14 +0530

    ad

--- hitesh/arrow.js
+++ hitesh/arrow.js
@@ -1,19 +1,41 @@
-const user = {
-  username: "sanjay",
-  price: 787,
-  welcomeMessage: function () {
-    console.log(`${this.username}, welcome to website`);
-    console.log(this);
-  },
-  
-};
+// const user = {
+//   username: "sanjay",
+//   price: 787,
+//   welcomeMessage: function () {
+//     console.log(`${this.username}, welcome to website`);
+//     console.log(this);
+//   },
+// };
 // user.welcomeMessage();
 // user.username="rakesh";
 // user.welcomeMessage();
-console.log(this);
+// console.log(this);
 
-function chai(){
-    let username="don";
-    console.log(this.username);
-}
-chai();
+// function chai(){
+//     let username="don";
+//     console.log(this.username);
+// }
+// chai()
+
+// const chai = () => {
+//   let username = "rakesh";
+//   console.log(this.username);
+// };
+// chai();
+
+// const adddd=(num1,num2)=>{
+//     return num1+num2;
+// }
+
+// const adddd = (num1, num2) => ({ username: "rakesh" });
+// console.log(adddd(4, 7))
+
+
+(function cahii() {
+  console.log(`db connected two`);
+})();
+
+
+((name)=>{
+    console.log(`db coonected ${name}`);
+})("san")
