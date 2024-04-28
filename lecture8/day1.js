a0c002a3a0a92e900aedc428cb608abeb4040fa9
rakesh1436 <sanjayrakeshgupta@gmail.com>
2024-04-28 17:47:01 +0530

    add

--- lecture8/day1.js
+++ lecture8/day1.js
@@ -6,3 +6,21 @@ const student={
     },
 
 };
+
+
+const employe={
+    calTax(){
+        console.log("tax rate is 100");
+    },
+    calTax2(){
+        console.log("tax rate is 100");
+
+        
+    }
+};
+const karanArjun={
+    salary:687,
+
+}
+
+karanArjun.__proto__=employe;
