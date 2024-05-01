d54b5e3a378a26311333037bdd3e5cae1b3116d3
rakesh1436 <sanjayrakeshgupta@gmail.com>
2024-05-01 17:17:01 +0530

    add

--- leture10/index.js
+++ leture10/index.js
@@ -1,6 +1,16 @@
 const URL = "http://universities.hipolabs.com/search?country=United+States";
+
+const factPara = document.querySelector("#data");
+const btn = document.querySelector("#btn");
+
 const getfacts = async () => {
   console.log("getting data... ");
   let response = await fetch(URL);
   console.log(response.status);
+  let data = await response.json();
+  let universities = data.map((university) => university.name);
+  factPara.innerText = universities.join( " name ",", ");
+
+ // factPara.innerText = universities.text;
 };
+btn.addEventListener("click", getfacts);
