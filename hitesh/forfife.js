a572361b0ff5e8e4f9b33d45eb1c276454451744
rakesh1436 <sanjayrakeshgupta@gmail.com>
2024-05-05 18:50:26 +0530

    add

--- hitesh/forfife.js
+++ hitesh/forfife.js
@@ -1,7 +1,17 @@
-const code=["java","js","python","butthom"]
-const item=code.forEach((obj)=>{
-    console.log(obj)
-    return code;
+// const code=["java","js","python","butthom"]
+// const item=code.forEach((obj)=>{
+//    // console.log(obj)
+//     return code;
 
-})
-console.log(item);
+// })
+// console.log(item);
+
+const num=[3,4,5,6,8];
+// const newnum=num.filter((nums)=>nums<4);
+// console.log(newnum);
+
+const my=num.filter((nums)=>{
+    return nums<5;
+
+});
+console.log(my)
