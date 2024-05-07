70f521dddb835fa3e8c07fee7a217fea8963aa65
rakesh1436 <sanjayrakeshgupta@gmail.com>
2024-05-07 23:32:10 +0530

    add

--- hitesh/forfife.js
+++ hitesh/forfife.js
@@ -6,12 +6,35 @@
 // })
 // console.log(item);
 
-const num=[3,4,5,6,8];
+// const num=[3,4,5,6,8];
 // const newnum=num.filter((nums)=>nums<4);
 // console.log(newnum);
 
-const my=num.filter((nums)=>{
-    return nums<5;
+// const my=num.filter((nums)=>{
+//     return nums>1;
 
-});
-console.log(my)
+// });
+// console.log(my)
+
+const number = [2, 4, 6, 7, 6, 8, 0];
+// const num2=[];
+// number.forEach((num) => {
+//   if (num > 4) {
+//     num2.push(num);
+//   }
+// });
+// console.log(num2);
+// const list = [];
+// const number1 = number.map((num) => num + 10);
+// console.log(number1);
+// const list = [];
+// const number1 = number.map((num) => {return num + 10});
+// console.log(number1);
+//chainig
+const lsit = number
+  .map((num) => num * 10)
+  .map((num) => num + 1)
+  .filter((num) => num >= 50);
+  
+console.log(lsit);
+lsit.forEach((list2)=>console.log(list2))
